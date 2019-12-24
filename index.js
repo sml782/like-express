e617395b9ddf86642caed7fe3d87b27923dff1b6
wb-sml406419 <wb-sml406419@alibaba-inc.com>
2019-12-24 20:21:42 +0800

    feat: 添加图标处理 & 错误捕获

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
-const http = require('http');
+// const http = require('http');
 const express = require('./lib/express');
 
 // 本次 http 请求的实例
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('请求开始...', req.method, req.url);
+  const { method = '', url = '/' } = req;
+  console.log(`${method} ${url}`)
   next();
+});
+
+app.get('/favicon.ico', (req, res, next) => {
+  console.log('请求logo');
+  // res.statusCode = 404;
+  res.writeHeader(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end();
 });
 
 app.use((req, res, next) => {
@@ -25,7 +33,7 @@ app.use('/api', (req, res, next) => {
 });
 
 app.get('/api', (req, res, next) => {
-  console.log('get /api 路由');
+  console.log('GET /api 路由');
   next();
 });
 
@@ -33,17 +41,28 @@ app.get('/api', (req, res, next) => {
 function loginCheck(req, res, next) {
   setTimeout(() => {
     console.log('模拟登陆成功');
-    next();
+    next('a');
   });
 }
 
-app.get('/api/get-cookie', loginCheck, (req, res, next) => {
-  console.log('get /api/get-cookie');
+app.get('/api/getCookie', loginCheck, (req, res, next) => {
+  console.log('GET /api/getCookie');
   res.json({
-    errno: 0,
+    success: true,
     data: req.cookie,
   });
 });
+
+// 每找到路由
+app.use((req, res, next) => {
+  next('你这啥啊，也没这路由啊!');
+});
+
+// 捕获错误
+app.use((err, req, res, next) => {
+  res.writeHeader(500, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end('<h1>无法访问此网站</h1>');
+})
 
 app.listen(8000, () => {
   console.log('server is running on port 8000');
